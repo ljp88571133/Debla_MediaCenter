4b3dfb44a54b2e18d85c16311527dd84fb7e6efb
ljp88571133 <1007909850@qq.com>
2015-03-20 15:23:08 +0800

    ZeYi
    write by Cocoonshu
    submit at 2015/3/20  15:23

---
+++ 2-Coding/1-Zeyi/Debla/src/com/debla/mediacenter/data/ImageItem.java
@@ -0,0 +1,16 @@
+package com.debla.mediacenter.data;
+
+/**
+ * 图像媒体对象
+ * @author Cocoonshu
+ * @date   2014-11-01 14:32:36
+ */
+public class ImageItem {
+	// TODO
+	
+	@Override
+	public String toString() {
+		// TODO
+		return super.toString();
+	}
+}
